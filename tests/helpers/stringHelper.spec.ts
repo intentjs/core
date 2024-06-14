f4ef7341ad00f8da3ae0a75447efaa8801a663a1
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-06-15 01:48:42 +0530

    added back the test file

---
+++ tests/helpers/stringHelper.spec.ts
@@ -0,0 +1,48 @@
+import { Str } from "../../lib/utils/strings";
+
+describe("String Helper", () => {
+  beforeEach(async () => {});
+
+  // it('should trim', () => {
+  //   expect(Str.trim(' vinayak ')).toBe('vinayak');
+  // });
+
+  it("should replace using string", () => {
+    const string = "vinayak don";
+    const find = "don";
+    const replace = "sarawagi";
+    expect(Str.replace(string, find, replace)).toBe("vinayak sarawagi");
+  });
+
+  it("should replace using regex", () => {
+    const string = "vinayak don";
+    const find = "don";
+    const replace = "sarawagi";
+    expect(Str.replace(string, find, replace)).toBe("vinayak sarawagi");
+  });
+
+  it("should replace all occurences using string", () => {
+    const string = "vinayak don don don don";
+    const find = "don";
+    const replace = "sarawagi";
+    expect(Str.replace(string, find, replace)).toBe(
+      "vinayak sarawagi sarawagi sarawagi sarawagi"
+    );
+  });
+
+  it("should replace all occurences using regex", () => {
+    const string = "vinayak don don don don";
+    const find = /don/g;
+    const replace = "sarawagi";
+    expect(Str.replace(string, find, replace)).toBe(
+      "vinayak sarawagi sarawagi sarawagi sarawagi"
+    );
+  });
+
+  it("should convert to snake case", () => {
+    const string = "IntentJs - For the devs_whoHaveTheIntent";
+    expect(Str.snake(string)).toBe(
+      "intent_js_for_the_devs_who_have_the_intent"
+    );
+  });
+});
