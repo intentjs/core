4bd1499be402796045da772595e4323954f4831e
chhabra1112 <piyushchhabra.1198@gmail.com>
2024-06-22 21:07:07 +0530

    CHANGE: name

--- lib/logger/index.ts
+++ lib/logger/index.ts
@@ -1,5 +1,6 @@
-import { LoggerService } from './service';
+import { LoggerService } from "./service";
+import * as winston from "winston";
 
-export function Logger(conn?: string) {
+export function Logger(conn?: string): winston.Logger {
   return LoggerService.getConnection(conn);
 }
