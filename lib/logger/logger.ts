2b7d446ca0a465fa261a72e045eeaf7aae0e506e
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-07-20 23:02:10 +0530

    test and refactor logger module

--- lib/logger/logger.ts
+++ lib/logger/logger.ts
@@ -1,6 +1,45 @@
-import winston from "winston";
-import { LoggerService } from "./service";
+import { LoggerService } from './service';
 
-export function Logger(conn?: string): winston.Logger {
-  return LoggerService.getConnection(conn);
-}
+export const Log = (conn?: string) => {
+  const logger = LoggerService.getConnection(conn);
+  return class {
+    constructor() {}
+
+    static emergency(payload: any) {
+      return logger.emerg(payload);
+    }
+
+    static alert(payload) {
+      return logger.alert(payload);
+    }
+
+    static critical(payload) {
+      return logger.crit(payload);
+    }
+
+    static error(payload) {
+      return logger.error(payload);
+    }
+
+    static warn(payload) {
+      return logger.warn(payload);
+    }
+
+    static notice(payload) {
+      return logger.notice(payload);
+    }
+
+    static info(payload) {
+      return logger.info(payload);
+    }
+
+    static debug(payload) {
+      return logger.debug(payload);
+    }
+  };
+};
+
+export const log = (payload: any, level?: string) => {
+  const logger = Log();
+  return logger[level ?? 'debug'](payload);
+};
