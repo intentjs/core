6f54260a4a46c22a0c6d46771aa51fa9604a32c7
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-09-16 22:45:34 +0530

    update container-factory import in foundation/index.ts

--- lib/foundation/index.ts
+++ lib/foundation/index.ts
@@ -2,3 +2,4 @@ export { Injectable, Inject, Optional } from '@nestjs/common';
 export * from './module-builder';
 export * from './service-provider';
 export * from './app-container';
+export * from './container-factory';
