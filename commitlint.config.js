dd479dea2b33b059b0a62345b178e98f36cc689d
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-11-11 06:38:25 +0530

    chore(monorepo): add commitlint and husky to the monorepo

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+};
+
+module.exports = Configuration;
