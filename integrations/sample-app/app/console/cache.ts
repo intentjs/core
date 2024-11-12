ef2cf1e11953faf6c2c4d90adee716f21cf6e1ba
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2024-11-12 22:31:44 +0530

    test(sample-app): intent command for testing out redis and in-memory cache

---
+++ integrations/sample-app/app/console/cache.ts
@@ -0,0 +1,30 @@
+import { Cache, Command, ConsoleIO } from '@intentjs/core';
+
+@Command('test:cache')
+export class TestCacheConsoleCommand {
+  async handle(_cli: ConsoleIO): Promise<boolean> {
+    _cli.info('Testing the cache');
+    const strValue = 'Hello world!';
+    const objValue = { msg: 'Hey there!' };
+
+    const disks = ['memory', 'redis'];
+    for (const disk of disks) {
+      _cli.info(`Testing integration for ${disk} cache`);
+      /**
+       * Set different values
+       */
+      await Cache.store(disk).set('str_value', strValue);
+      await Cache.store(disk).set('obj_value', objValue);
+
+      /**
+       * Get value from differen
+       */
+      const strValueFromCache = await Cache.store(disk).get('str_value');
+      const objValueFromCache = await Cache.store(disk).get('obj_value');
+      console.log(strValueFromCache);
+      console.log(objValueFromCache);
+    }
+
+    return true;
+  }
+}
