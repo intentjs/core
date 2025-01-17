5c21e6f762a32a4eac90b6f197359afacf2e014f
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2025-01-17 23:29:50 +0530

    fix(core): strict path bug

--- packages/core/lib/rest/helpers.ts
+++ packages/core/lib/rest/helpers.ts
@@ -1,2 +1,7 @@
-export const joinRoute = (...paths: string[]) =>
-  paths.join('/').replace(/[/]+/g, '/');
+export const joinRoute = (...paths: string[]) => {
+  const methodPath = paths[2];
+  if (!methodPath) {
+    return paths.slice(0, 2).join('/').replace(/\/+/g, '/');
+  }
+  return paths.join('/').replace(/\/+/g, '/');
+};
